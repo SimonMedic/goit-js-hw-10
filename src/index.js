55af68f41f72eb78f6bd9894d2ff7b3eeb0ef985
SimonMedic <szymonpaluch@hotmail.com>
2024-02-27 16:44:09 +0100

    up

---
+++ src/index.js
@@ -0,0 +1,88 @@
+import { fetchBreeds, fetchCatByBreed } from './cat-api';
+
+import SlimSelect from 'slim-select';
+import 'slim-select/dist/slimselect.css';
+
+import Notiflix from 'notiflix';
+
+document.addEventListener("DOMContentLoaded", async () => {
+    const loader = document.querySelector(".loader");
+    const errorDisplay = document.querySelector(".error");
+    const catInfo = document.querySelector(".cat-info");
+    const breedSelect = document.querySelector("single");
+
+    try {
+        loader.style.display = "block";
+
+        const breeds = await fetchBreeds();
+
+        if (breeds.length === 0) {
+            Notiflix.Notify.failure('No cat breeds found. Please try reloading the page.');
+            loader.style.display = "none";
+            return;
+        }
+        const options breeds.map(breed => ({
+            value: breed.id,
+            text: breed.name
+        }));
+
+        options.unshift({
+            value: '',
+            text: '- Select a breed -'
+        });
+
+        new SlimSelect({
+            select: '#single',
+            data: options
+        });
+
+        loader.style.display = "none";
+    }
+    catch (error) {
+        console.error("Error while fetching cat breeds:", error);
+        Notiflix.Notify.failure('Oops! Something went wrong! Try reloadnig the page!');
+    }
+
+    breedSelect.addEventListener("change", async event => {
+        const breedId = event.target.value;
+        loader.style.display = "block";
+        errorDisplay.style.display = "none";
+        catInfo.innerHTML = "";
+        catInfo.style.display = "none";
+
+        try {
+            const catData = await fetchCatByBreed(breedId);
+
+            if (!catData || catData.length === 0) {
+                throw new Error("No cat data found.");
+            }
+            
+            document.querySelector("body").classList.add("selected-background");
+
+            const catImg = document.createElement("img");
+            catImg.src = catData[0].url;
+            catInfo.appendChild(catImg);
+
+            const catName = document.createElement("p");
+            catName.innerHTML = `<strong>Name:</strong> ${catData[0].breeds[0].name}`;
+            catInfo.appendChild(catName);
+
+            const catDescription = document.createElement("p");
+            catDescription.innerHTML = `<strong>Description:</strong> ${catData[0].breeds[0].description}`;
+            catInfo.appendChild(catDescription);
+
+            const catTemperament = document.createElement("p");
+            catTemperament.innerHTML = `<strong>Temperament:</strong> ${catData[0].breeds[0].temperament}`;
+            catInfo.appendChild(catTemperament);
+
+            catInfo.style.display = "block";
+        }
+        catch (error) {
+            console.error("Error while fetching cat information:", error);
+            Notiflix.Notify.failure('Oops! Something went wrong! Try reloading the page!');
+        }
+        finally {
+            loader.style.display = "none";
+        }
+    });
+});
